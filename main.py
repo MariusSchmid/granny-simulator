050748a6303a3d2344256381c3b918974df9be38
Marius <mafloschmid@aol.com>
2023-12-03 16:00:07 +0100

    browser specified in environment can be used

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ import sys
 import webbrowser
 import hotkey_manager as hkm
 
-
 def menu():
     print("Menü: ")
     print("1. Google Suche ")
     print("2. YouTube Video schauen ")
 
+
+def open_browser_url(url):
+    specific_browser_path =  os.environ.get('BROWSER_PATH')
+    if specific_browser_path:
+        print(specific_browser_path)
+        webbrowser.get(f"{specific_browser_path} %s").open(url)
+    else:
+        webbrowser.open(url)
+
 def open_google():
     print("Google Suche")
     search_query = input("Bitte geben sie ein nach was sie suchen wollen:")
-    google_url = f"https://www.google.com/search?q={search_query}"
-    webbrowser.open(google_url)
+    open_browser_url(f"https://www.google.com/search?q={search_query}")
+
 
 def open_youtube():
     search_query = input("Bitte geben sie ein nach was sie suchen wollen:")
-    youtube_url = f"https://www.youtube.com/results?search_query={search_query}"
-    webbrowser.open(youtube_url)
+    open_browser_url(f"https://www.youtube.com/results?search_query={search_query}")
 
 def escape_all():
     os.system("taskkill /IM chrome.exe /F")
@@ -28,6 +35,7 @@ def escape_all():
 
 def main():
     hkm.listen_to_hotkeys(escape_all)
+
 
     while True:
         menu()
