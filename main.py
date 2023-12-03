afdd2512e3d966d142fe2a27e90e9426b9e4a686
Marius <mafloschmid@aol.com>
2023-12-03 15:10:11 +0100

    hotkey is working for esc

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import webbrowser
+import threading
 from pynput import keyboard
+
+
+def hotkey_thread_function(name):
+    with keyboard.GlobalHotKeys({
+            '<27>': escape_all}) as h: #27 = ESC
+        h.join()
+
 
 def menu():
     print("Menü: ")
     print("1. Google Suche ")
     print("2. YouTube Video schauen ")
 
+    
+
 
 def open_google():
     print("Google Suche")
-
     search_query = input("Bitte geben sie ein nach was sie suchen wollen:")
-
     google_url = f"https://www.google.com/search?q={search_query}"
-
     webbrowser.open(google_url)
 
 
 def open_youtube():
-
     search_query = input("Bitte geben sie ein nach was sie suchen wollen:")
-
     youtube_url = f"https://www.youtube.com/results?search_query={search_query}"
-
     webbrowser.open(youtube_url)
 
 def escape_all():
@@ -30,14 +34,11 @@ def escape_all():
 
 def main():
 
-    with keyboard.GlobalHotKeys({
-            'n': escape_all() })  as h:
-        h.join()
-
+    hotkey_thread = threading.Thread(target=hotkey_thread_function, args=(1,))
+    hotkey_thread.start()
     while True:
         menu()
         choice = input("Bitte wählen sie einen Menüpunkt aus!")
-
         if choice == "1":
             open_google()
         elif choice == "2":
