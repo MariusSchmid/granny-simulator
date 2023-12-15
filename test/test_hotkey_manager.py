eb6220ad811d13beb0a5c359c92c710afbfe8280
Marius <mafloschmid@aol.com>
2023-12-15 18:31:10 +0100

    unittests: added unit test for hot key manager

--- test/test_hotkey_manager.py
+++ test/test_hotkey_manager.py
@@ -1,33 +1,7 @@
-# import granny_simulator as gs
+import granny_simulator as gs
 
-# class TestStringMethods(unittest.TestCase):
+def test_thread_started(mocker):
+    mocker.patch('granny_simulator.hotkey_manager.threading.Thread.start',return_value=None)
+    gs.listen_to_hotkeys(None)
+    gs.hotkey_manager.threading.Thread.start.assert_called_once()
 
-#     @mock.patch('granny_simulator.hotkey_manager.keyboard')
-#     def test_upper(self, keyboard):
-#         # pass
-#         # gs.listen_to_hotkeys(None)
-#         pass
-
-#     def test_isupper(self):
-#         self.assertTrue('FOO'.isupper())
-#         self.assertFalse('Foo'.isupper())
-
-#     def test_split(self):
-#         s = 'hello world'
-#         self.assertEqual(s.split(), ['hello', 'world'])
-#         # check that s.split fails when the separator is not a string
-#         with self.assertRaises(TypeError):
-#             s.split(2)
-
-# if __name__ == '__main__':
-#     unittest.main()
-
-
-from unittest import TestCase
-
-class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    def test_always_fails(self):
-        self.assertTrue(False)
